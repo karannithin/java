de18f45271f72fccda8a517de6e945ab0638a6f0
karannithin <karannithin19@gmail.com>
2025-02-07 14:23:13 +0530

    day 6 commit

---
+++ wipro_java_project/src/com/wipro/java/junit/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.wipro.java.junit;
+
+import org.junit.Test;
+
+// Test class for Calculator
+public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+    
+    @Test
+    public void testAdd() {
+        int result = calculator.add1(3, 2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        int result = calculator.subtract(5, 3);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testAddWithNegativeNumbers() {
+        int result = calculator.add1(-3, -2);
+        assertEquals(-5, result);
+    }
+
+    @Test
+    public void testSubtractWithNegativeNumbers() {
+        int result = calculator.subtract(-3, -2);
+        assertEquals(-1, result);
+    }
+
+	private void assertEquals(int i, int result) {
+		// TODO Auto-generated method stub
+		
+	}
+}
