19c29262f476b6f88e34bbccb1c19c4d18ce414e
karannithin <karannithin19@gmail.com>
2025-02-18 22:04:30 +0530

    Day 15 commit

---
+++ wipro_java_project/src/com/wipro/java/linkedcomparator/Comparator.java
@@ -0,0 +1,11 @@
+package com.wipro.java.linkedcomparator;
+
+import java.util.Comparator;
+
+class AgeComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person p1, Person p2) {
+        return Integer.compare(p1.age, p2.age);
+    }
+}
+
